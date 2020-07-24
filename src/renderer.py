0fe5445d9a89debaea6f4d8aa511eb29fafa040d
stemar94 <marvin.stenger@t-online.de>
2020-07-24 19:07:19 +0200

    remove Renderer initialization for now

--- src/renderer.py
+++ src/renderer.py
@@ -9,8 +9,9 @@ class Renderer(object):
     # TODO: Discuss how to handle exporting/building
 
     def __init__(self):
-        self.engine = Environment(loader=PackageLoader('moxnet', 'templates'), autoescape=select_autoescape(['html']))
-        self.template = self.engine.get_template('page.html')
+        pass
+        #self.engine = Environment(loader=PackageLoader('moxnet', 'templates'), autoescape=select_autoescape(['html']))
+        #self.template = self.engine.get_template('page.html')
 
     def render(self, sites: [Website], path: str = None):
         pass
