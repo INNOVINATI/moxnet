f09532962998b2c714422cd57cad944d3111cbc7
Maximilian Wolf <maximilian.wolf@innovinati.com>
2020-07-24 18:32:39 +0200

    Refactored Page.build()

--- src/renderer.py
+++ src/renderer.py
@@ -7,11 +7,10 @@ from src.website import Website
 
 class Renderer(object):
     # TODO: Discuss how to handle exporting/building
-    folder = os.path.join(os.getcwd(), 'html')
 
     def __init__(self):
         self.engine = Environment(loader=PackageLoader('moxnet', 'templates'), autoescape=select_autoescape(['html']))
         self.template = self.engine.get_template('page.html')
 
-    def render(self, sites: [Website]):
+    def render(self, sites: [Website], path: str = None):
         pass
