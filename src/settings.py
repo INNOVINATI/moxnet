b485e6ba37566ba950b63d0d4ccef1e6d25be4f4
Maximilian Wolf <maximilian.wolf@innovinati.com>
2020-07-26 22:09:24 +0200

    Fixed merge

--- src/settings.py
+++ src/settings.py
@@ -1,10 +1,4 @@
 class Settings(object):
-
-<<<<<<< HEAD
-    def __init__(self, domains: int = 5, pages: int = 50, link_sites: bool = False):
-        self.domains = domains
-        self.pages = pages
-=======
     def __init__(self, num_domains: int = 1, num_pages: int = 1, num_externals: int = 1, link_sites: bool = False):
         if not isinstance(num_pages / num_domains, int):
             raise Exception(
@@ -12,5 +6,4 @@ class Settings(object):
         self.num_domains = num_domains
         self.num_pages = num_pages
         self.num_externals = num_externals
->>>>>>> dbb5199430e6182837f367895a073a0ac883ddab
         self.interlink = link_sites
