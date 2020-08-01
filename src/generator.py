3ecde02153687dc4870b251d210fe1a9665479da
Maximilian Wolf <maximilian.wolf@innovinati.com>
2020-08-02 00:38:27 +0200

    Prepared generation of NGINX configuration (improved)

--- src/generator.py
+++ src/generator.py
@@ -8,22 +8,24 @@ from src.website import Website
 
 class Generator(object):
     sites = dict()
-    server = ''
 
     def __init__(self):
         self.engine = Environment(
             loader=FileSystemLoader(os.path.join(os.path.dirname(__file__), 'templates')),
             autoescape=select_autoescape(['html'])
         )
-        self.template = self.engine.get_template('page.html')
+        self.page_template = self.engine.get_template('page.html')
+        self.server_template = self.engine.get_template('nginx.conf')
 
     def generate(self, sites: [Website], path: str = None):
         export_to = path if path else os.path.join(os.getcwd(), 'moxnet')
-        self.sites = list(map(lambda site: list(map(lambda page: self.template.render(page=page), site.pages)), sites))
-        template = self.engine.get_template('page.html')
+        self.sites = list(map(lambda site: list(map(lambda page: self.page_template.render(page=page), site.pages)), sites))
+        page_template = self.engine.get_template('page.html')
+        server_template = self.server_template.render(sites=sites)  #TODO: Implement file writing
         for site in sites:
             for page in site.pages:
-                p = template.render(page=page)
+                p = page_template.render(page=page)
+                #TODO: Pull the check outside of the loops
                 f = os.path.join(export_to, site.domain, f'page{page.id}.html')
                 if not os.path.exists(os.path.dirname(f)):
                     try:
@@ -33,4 +35,3 @@ class Generator(object):
                             raise
                 with open(f, "w") as f:
                     f.write(p)
-
